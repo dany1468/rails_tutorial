46aa5d1b23eff2e57115f31e88a61f875a134051
dany1468 <dany1468@gmail.com>
2019-10-29 06:54:55 +0900

    [dotnet] dotnet ef migrations add AddMicroPostTable

---
+++ dotnet/ToyApp/Migrations/20191028215425_AddMicroPostTable.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ToyApp.Migrations
+{
+    public partial class AddMicroPostTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MicroPost",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Content = table.Column<string>(nullable: true),
+                    UserID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MicroPost", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MicroPost");
+        }
+    }
+}
