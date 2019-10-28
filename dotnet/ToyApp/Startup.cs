c7be0a164ff647304a141f1828da86427df6b93a
dany1468 <dany1468@gmail.com>
2019-10-28 11:16:16 +0900

    [dotnet] Add DbContext to dependency injection targets

--- dotnet/ToyApp/Startup.cs
+++ dotnet/ToyApp/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ToyApp.Models;
 
 namespace ToyApp
 {
@@ -24,6 +26,9 @@ namespace ToyApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<ToyContext>(options =>
+                options.UseSqlite(Configuration.GetConnectionString("ToyContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
