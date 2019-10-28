ecacf6a4385e722c4f06e2d7bdc06293cd5fd269
dany1468 <dany1468@gmail.com>
2019-10-29 07:33:08 +0900

    [dotnet] Add validates for content length of micropost

--- dotnet/ToyApp/Models/MicroPost.cs
+++ dotnet/ToyApp/Models/MicroPost.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace ToyApp.Models
 {
     public class MicroPost
     {
         public int ID { get; set; }
+        [StringLength(140)]
         public string Content { get; set; }
         public int UserID { get; set; }
     }
