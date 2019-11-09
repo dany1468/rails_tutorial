e976e77c063da4def7e0452f1da31900b5ab1eea
dany1468 <dany1468@gmail.com>
2019-11-09 09:51:37 +0900

    [rails] Add validates for email and name  presence of user

--- rails/toy_app/app/models/micropost.rb
+++ rails/toy_app/app/models/micropost.rb
@@ -1,4 +1,6 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   validates :content, length: {maximum: 140}, presence: true
+  validates :email, presence: true
+  validates :name, presence: true
 end
