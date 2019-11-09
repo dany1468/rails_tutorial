31f451770f1227bd08e1a6c4ca6d7aeb6ebc88cb
dany1468 <dany1468@gmail.com>
2019-11-09 09:51:55 +0900

    [dotnet] Add validates for email and name presence of user

--- dotnet/ToyApp/Models/User.cs
+++ dotnet/ToyApp/Models/User.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ToyApp.Models
 {
     public class User
     {
         public int ID { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Email { get; set; }
 
         public ICollection<MicroPost> MicroPosts { get; set; }
