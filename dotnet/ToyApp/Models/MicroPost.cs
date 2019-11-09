7b3bd460b7dcea70b47f3b91fd23c7b1ab44580e
dany1468 <dany1468@gmail.com>
2019-11-09 09:48:53 +0900

    [dotnet] Add validates for content presence of micropost

--- dotnet/ToyApp/Models/MicroPost.cs
+++ dotnet/ToyApp/Models/MicroPost.cs
@@ -6,7 +6,7 @@ namespace ToyApp.Models
     public class MicroPost
     {
         public int ID { get; set; }
-        [StringLength(140)]
+        [StringLength(140), Required]
         public string Content { get; set; }
         public int UserID { get; set; }
 
